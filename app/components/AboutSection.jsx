86c10e5c90d3fc72d3fb9161016be882881d3ef9
joscaz <joscazertuche@gmail.com>
2023-10-26 09:50:42 -0600

    Added TabButton component

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -1,10 +1,48 @@
 "use client";
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>React</li>
+        <li>Javascript</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>PrepaTec</li>
+        <li>Tecnológico de Monterrey</li>
+        <li>Bachelor of Science - BS, Computer Science and Technology</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>QPR Suicide Prevention Gatekeeper Program</li>
+        <li>How to collaborate with Github? Workshop</li>
+        <li>ICPC Training Camp 2022</li>
+      </ul>
+    ),
+  },
+];
 
 const AboutSection = () => {
   const [tab, setTab] = useState("skills");
-  const [startTransition, isPending] = useTransition();
+  const [isPending, startTransition] = useTransition();
 
   const handleTabChange = (id) => {
     startTransition(() => {
@@ -21,7 +59,7 @@ const AboutSection = () => {
         width={400}
         height={400}
       />
-      <div>
+      <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
         <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
         <p className="text-base md:text-lg">
           I am a full stack software engineer with a lot of habilities for
@@ -31,13 +69,30 @@ const AboutSection = () => {
           full stack software engineer with a lot of habilities for programming
           stufff and I need to later change all of this so it apperars good.
         </p>
-        <div className="flex flex-row mt-8">
-          <span className="mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-purple-500">
-            Skills
-          </span>
-          <span>Education</span>
-          <span>Experience</span>
+        <div className="flex flex-row justify-start mt-8">
+          <TabButton
+            selectedTab={() => handleTabChange("skills")}
+            active={tab === "skills"}
+          >
+            {" "}
+            Skills{" "}
+          </TabButton>
+          <TabButton
+            selectedTab={() => handleTabChange("education")}
+            active={tab === "skills"}
+          >
+            {" "}
+            Education{" "}
+          </TabButton>
+          <TabButton
+            selectedTab={() => handleTabChange("certifications")}
+            active={tab === "skills"}
+          >
+            {" "}
+            Certifications{" "}
+          </TabButton>
         </div>
+        <div className="mt-8">{TAB_DATA.find((t) => t.id === tab).content}</div>
       </div>
     </section>
   );
